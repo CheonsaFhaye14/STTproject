b92ceda7c86b6b098338877161fe19bfd19c458a
Angel Fhaye Rafanan <angelfhayeyrafanan@iskolarngbayan.pup.edu.ph>
2026-04-14 21:13:01 +0800

    pages and navbar

---
+++ Components/Models/SubDistributor.cs
@@ -0,0 +1,11 @@
+﻿namespace STTproject.Components.Models
+{
+    public class SubDistributor
+    {
+        public int SubdId { get; set; }
+        public int SubdCode { get; set; }
+        public int SubdId { get; set; }
+
+        public int SubdId { get; set; }
+    }
+}
